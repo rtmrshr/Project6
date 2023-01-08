2de05ba3e51725e82d74ab468343028bb2e35b83
rtmrshr <ratmir.sher@gmail.com>
2023-01-08 18:19:17 +0200

    Initial Commit

---
+++ Project6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6
+//
+//  Created by  ratmir on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
